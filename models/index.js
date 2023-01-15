363bfbdc15d4871cb68f3b900d14c40f987e7ad8
E Cussler <cusslere@gmail.com>
2023-01-15 14:03:42 -0800

    Defined columns in Tag and ProductTags, wrote associations in index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Establishes "parent" one-to-many relationship between Category and Product
+Category.hasMany(Product, {
+  foreignKey: 'category_id', 
+  onDelete: 'CASCADE', 
+}); 
 
-// Categories have many Products
+// Establishes "child" one-to-many relationship between Category and Product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', 
+}); 
 
-// Products belongToMany Tags (through ProductTag)
+// Establishes many-to-many relationship between Product and Tag through the pre-defined columns in ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id'
+}) 
 
-// Tags belongToMany Products (through ProductTag)
+// Establishes many-to-many relationship between Product and Tag through the pre-defined columns in ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag
+  foreignKey: 'tag_id', 
+})
 
 module.exports = {
   Product,
