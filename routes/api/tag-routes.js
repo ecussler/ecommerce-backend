71d34c94f14bbce751435b8f370da904ebf213a0
E Cussler <cusslere@gmail.com>
2023-01-16 15:26:34 -0800

    Updated all tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,74 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+
+// GET All tags
+router.get('/', async (req, res) => {
+  try {
+    const allTags = await Tags.findAll({
+      include: [{ model: Product }, { model: ProductTag }], 
+    }); 
+    res.status(200).json(allTags)
+  } catch (err) {
+    res.status(500).json(err); 
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+
+// GET tag by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }], 
+    }); 
+
+    if (!tagData) {
+      res.status(400).json({ message: 'No tag found with that ID!'}); 
+    }
+
+    res.status(200).json(tagData); 
+  } catch (err) {
+    res.status(500).json(err); 
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+
+
+router.post('/', async (req, res) => {
+  const tagData = await Tag.create(req.body); 
+
+  return res.json(tagData); 
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+
+router.put('/:id', async (req, res) => {
+  const tagData = await Tag.update(
+    {
+      id: req.body.id, 
+      tag_name: req.body.tag_name
+    }, 
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  ); 
+
+  // Having trouble getting the updated category to display, reflected properly in all categories list
+  return res.json(tagData)
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+
+
+router.delete('/:id', async (req, res) => {
+  const tagData = await Tag.destroy({
+    where: {
+      id: req.params.id
+    },
+  });
+
+  // Having trouble getting the updated category to display, reflected properly in all categories list
+  return res.json(tagData); 
 });
 
 module.exports = router;
